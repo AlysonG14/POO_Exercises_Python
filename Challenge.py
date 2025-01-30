c29efa8141a87445e4ea03328c2e845b15944cfb
AlysonPereira <alyson.bosch.senai14@gmail.com>
2025-01-30 16:16:26 -0300

    Add files via upload

---
+++ Challenge.py
@@ -0,0 +1,3 @@
+'''
+Challenge - Implemente um sistema de gerenciamento de eventos, como um software para planejar e organizar eventos corporativos, casamentos, festas, etc. O sistema deve gerenciar o agendamento de eventos, as tarefas associadas a cada evento, os fornecedores e a distribuição de responsabilidades.
+'''
